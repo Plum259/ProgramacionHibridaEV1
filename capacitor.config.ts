f34c884622ba86da9422f7effe949b27283f8ede
Plum259 <plumcoyote@gmail.com>
2023-12-14 16:27:18 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.evaluacion1',
+  appName: 'evaluacion1',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
